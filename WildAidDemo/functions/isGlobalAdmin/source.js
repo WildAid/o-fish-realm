30e33a322d3b0c60508d396382531cc7ad8cd8e5
Andrew Morgan <andrew.morgan@mongodb.com>
2020-07-28 14:27:30 +0100

    More robust check if a User document shows it's a global.admin

--- WildAidDemo/functions/isGlobalAdmin/source.js
+++ WildAidDemo/functions/isGlobalAdmin/source.js
@@ -9,6 +9,15 @@ exports = function(emailAddress){
   return userCollection.findOne({email: emailAddress})
   .then ( userDoc => { 
     console.log(`${emailAddress} is a global admin? - ${userDoc.global && userDoc.global.admin}`);
-    return (userDoc.global && userDoc.global.admin);
+    if (userDoc.global) {
+        if (userDoc.global.admin) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        console.log("Missing global attribute from User document");
+        return false;
+      }
   }).catch( e => { console.log(e); return false; }); 
 };
