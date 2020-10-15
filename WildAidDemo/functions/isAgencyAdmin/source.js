a4d871788a99bcd2a906b510faafab3c8dbe1266
Sheeri Cabral <awfief@gmail.com>
2020-10-15 12:03:43 -0400

    check agency name against agency.name, not agency
    Found this bug while investigating why agency admins could not create users, but global admins could.

--- WildAidDemo/functions/isAgencyAdmin/source.js
+++ WildAidDemo/functions/isAgencyAdmin/source.js
@@ -1,9 +1,9 @@
 exports = function(agency, emailAddress){
-  console.log(`Checking email address: ${emailAddress} for agency: ${agency}`);
+  console.log(`Checking email address: ${emailAddress} for agency: ${agency.name}`);
   var userCollection = context.services.get("mongodb-atlas")
     .db("wildaid").collection("User");
   
-  return userCollection.findOne({email: emailAddress, agency: {name: agency, admin: true}})
+  return userCollection.findOne({email: emailAddress, agency: {name: agency.name, admin: true}})
   .then ( userDoc => { 
     if (userDoc) {
       console.log('Matches Agency Admin rule');
